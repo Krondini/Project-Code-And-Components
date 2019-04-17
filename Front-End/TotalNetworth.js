ca1d3d2410af6e2a67da2f2f928fdbc8e9016fc4
Krondini <koro5940@colorado.edu>
2019-04-16 20:14:29 -0600

    Added JavaScript to calculate networth for Home Page

--- Front-End/TotalNetworth.js
+++ Front-End/TotalNetworth.js
@@ -1,11 +1,13 @@
 function Calculate_Networth()
 {
-	var table = document.getElementById("stats_table");
+
+	var table = 
+		$.get('edit.html', 'stats_table');
 	var row_counter;
 	var total_Networth;
 	for (row_counter = 1; row_counter < table.rows.length; row_counter++)
 	{
-		total_Networth += table.rows[row_counter].cells[1].innerHTML;
+		total_Networth += parseFloat(table.rows[row_counter].cells[1].innerHTML);
 	}
 	return total_Networth;
 }
