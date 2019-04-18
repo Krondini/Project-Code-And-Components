2e354fc0f676e4e263e6bdacbd5efb8518e97aab
Dylan Sain <dylanray.sain@gmail.com>
2019-04-17 19:31:15 -0600

    edits to server.js

--- NetWorthAPP/server.js
+++ NetWorthAPP/server.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const app = express()
 
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
+var pgp = require('pg-promise')();
+
+const dbConfig = {
+	host: 'localhost',
+	port: 5432,
+	database: 'net_worth_db',
+	user: 'postgres',
+	password: 'pass'
+};
+
+var db = pgp(dbConfig);
 
 app.get('/home', function (req, res) {
-  res.render('home');
+  var query = "select * from netWorthInfo;";
+  db.any(query)
+        .then(function (rows) {
+            res.render('home',{
+				my_title: "Home Page",
+				data: rows
+			})
+        })
+	.catch(function (err) {
+            // display error message in case an error
+            request.flash('error', err);
+            response.render('home', {
+                title: 'Home Page Error',
+                data: ''
+            })
+        })
 })
 
 app.get('/add', function (req, res){
