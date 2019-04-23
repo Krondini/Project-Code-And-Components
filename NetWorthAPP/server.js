ed19ed29e698f87aef328894e13b775939bc6902
Krondini <koro5940@colorado.edu>
2019-04-23 09:56:39 -0600

    Small change to createAccount.html

--- NetWorthAPP/server.js
+++ NetWorthAPP/server.js
@@ -6,6 +6,10 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 var pgp = require('pg-promise')();
 
+var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
+app.use(bodyParser.json());              // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 const dbConfig = {
   host: 'localhost',
   port: 5432,
@@ -16,14 +20,37 @@ const dbConfig = {
 
 var db = pgp(dbConfig);
 
+var ID = 0;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/'));//This line is necessary for us to use relative paths and access our resources directory
+
+
 
 // Redirection because we're lazy
 app.get('/', function(req,res){
-	res.redirect('/home');
+  res.redirect('/home');
 })
 
+
+app.get('/share', function (req, res){
+  res.render('share');
+})
+
+app.get('/login', function (req, res){
+  res.render('login');
+})
+
+app.get('/create_account', function (req, res){
+  res.render('createAccount');
+
+})
+
+
+
 app.get('/home', function (req, res) {
-  var query = "select * from networthinfo where userid = '1';";
+  var query = "select * from networthinfo where userid = '" + ID + "';";
   db.any(query)
     .then(function (rows) { 
   console.log(rows)
@@ -34,13 +61,14 @@ app.get('/home', function (req, res) {
         })
   .catch(function (err) {
             // display error message in case an error
-            request.flash('error', err);
-            response.render('home', {
+            req.flash('error', err);
+            res.render('home', {
                 title: 'Home Page Error',
                 data: ''
             })
         })
 })
+
 
 app.get('/add', function (req, res){
   var query = "select * from itemsentered where userid = '1';";
@@ -54,6 +82,8 @@ app.get('/add', function (req, res){
   })
 })
 
+
+
 app.get('/edit', function (req, res){
   var query = "select * from itemsentered where userid = '1';";
   db.any(query)
@@ -66,58 +96,95 @@ app.get('/edit', function (req, res){
   })
 })
 
-app.get('/create_account', function (req, res){
-	res.render('createAccount');
 
+app.post('/create_account/add_user',function (req, res){
+
+  var usr = req.body.username;
+  var pass = req.body.password;
+  var email_ = req.body.email;
+  console.log(usr);
+  console.log(pass);
+  console.log(email_);
+
+  //var user_query = 'SELECT * FROM userInfo;';
+  var user_query = "SELECT username FROM userInfo WHERE username = '"+ usr + "';";
+  var email_query = "SELECT email FROM userInfo where email = '" + email_ +"';";
+
+  db.task('get-everything', task => {
+    return task.batch([
+      task.any(user_query),
+      task.any(email_query)
+      ]);
+  })
+  .then(info => {
+    res.redirect('/create_account');
+    console.log('it worked!!!!!!!!!!!!!!!!');
+  })
+  .catch(error => {
+    res.redirect('/create_account');
+    console.log('there was an error!!!');
+})
 })
 
+app.post('/login/verify', function (req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+  var query = "SELECT username, password, userID FROM userInfo WHERE username = '" + username + "' AND password = '" + password + "';";
+  db.any(query)
+    .then(function (rows) { 
+        if(rows.length == 1){
+      ID = rows[0].userid;
+      res.redirect('/home');
+  }
+  else{
+    res.redirect('/login');
+  }
+})
+})
+
+
+/*
 app.post('/create_account', function (req, res){
-	// Retrive information from form
-	var user = req.body.username,
-		pass = req.body.password,
-		email_ = req.body.email;
-	
-	// Retrive possible queries
-	var user_query = 'SELECT username FROM userInfo WHERE username = user;',
-		email_query = 'SELECT email FROM userInfo WHERE email = email_;';
-	
-	// Compare form info to queries
-	// If info does match anything in DB
-	// Send an alert and repromt form
-	db.task('get-everything', task => {
-		return task.batch([
-			task.any(user_query),
-			task.any(email_query)
-			]);
-	})
-	.then(function (user, pass, email_) {
-		if (user == user_query)
-		{
-			alert('This username is already taken');
-			res.render('create_account');
-		} 
-		else if(email_ == email_query)
-		{
-			alert('This email address is already taken');
-			res.render('create_account');
-		}
-		else{
-			alert('Account created!');
-		}
-	})
-	.catch(function (err){
-		request.flash('Error', err);
-		res.redirect('/home');
-	})
-})
+  // Retrive information from form
+  var user = req.body.username;
+  var pass = req.body.password;
+  var email_ = req.body.email;
+  
+  // Retrive possible queries
+  var user_query = 'SELECT username FROM userInfo WHERE username = user;';
+    email_query = 'SELECT email FROM userInfo WHERE email = email_;';
+  
+  // Compare form info to queries
+  // If info does match anything in DB
+  // Send an alert and repromt form
+  db.task('get-everything', task => {
+    return task.batch([
+      task.any(user_query),
+      task.any(email_query)
+      ]);
+  })
+  .then(function (user, pass, email_) {
+    if (user == user_query)
+    {
+      alert('This username is already taken');
+      res.render('create_account');
+    } 
+    else if(email_ == email_query)
+    {
+      alert('This email address is already taken');
+      res.render('create_account');
+    }
+    else{
+      alert('Account created!');
+    }
+  })
+  .catch(function (err){
+    req.flash('Error', err);
+    res.redirect('/home');
+  });
+});
+*/
 
-app.get('/share', function (req, res){
-  res.render('share');
-})
-
-app.get('/login', function (req, res){
-  res.render('login');
-})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
