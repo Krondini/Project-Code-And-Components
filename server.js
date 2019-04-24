7b6069fb9461c65ab56ff6377cecbc0d9422748e
NinoZhongzhiZhang <46726967+NinoZhongzhiZhang@users.noreply.github.com>
2019-04-23 21:29:31 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// import alert from 'alert-node'
-
-
 const express = require('express')
 const app = express()
 
@@ -13,12 +10,15 @@ var bodyParser = require('body-parser'); //Ensure our body-parser tool has been 
 app.use(bodyParser.json());              // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+
+
+
 const dbConfig = {
   host: 'localhost',
   port: 5432,
   database: 'net_worth_db',
   user: 'postgres',
-  password: 'pass'
+  password: 'XIAOxiao1998!'
 };
 
 var db = pgp(dbConfig);
@@ -37,151 +37,6 @@ app.get('/', function(req,res){
 })
 
 
-app.get('/share', function (req, res){
-  res.render('share',{
-  	failed : false
-  })
-})
-
-app.post('/share', function (req, res){
-  console.log(req.body.email);
-  console.log(req.body.comment);
-
-  var selector1 = null;
-  var selector2 = null;
-  var selector3 = null;
-
-  if (req.body.selector1 != undefined)
-  {
-  	selector1 = req.body.selector1;
-  }
-
-  if (req.body.selector2 != undefined)
-  {
-  	selector2 = req.body.selector2;
-  }
-
-  if (req.body.selector3 != undefined)
-  {
-  	selector3 = req.body.selector3;
-  }
-
-//install nodemailer first
-//enter: npm install nodemailer  
-
-  	var nodemailer = require('nodemailer'); 
-
-	var transporter = nodemailer.createTransport({
-	  service: 'gmail',
-	  auth: {
-	    user: 'netWorthAppShare@gmail.com', //We need to create a public email address to send the information;
-	    pass: '123networth'
-	  }
-	});
-
-	var targetemail = req.body.email;
-	var targetcomment = req.body.comment;
-
-	var user_email = "select email from userInfo where userID = '"+ ID +"';";
-	db.query(user_email)
-		.then(rows => {
-			console.log(rows[0]);
-		})
-		.catch( err => throw (err))
-	var send_text = "";
-	var user_name = "select firstName from userInfo where userID = '" + ID + "';" + ' ' + "select lastName from userInfo userID = '" + ID + "';";
-	var net_worth = "select netWorth from netWorthInfo where userID = '" + ID + "';";
-	console.log(net_worth);
-	var total_assets = "select totalAssets from netWorthInfo where userID = '" + ID + "';";
-	var total_liabilities = "select totalLiabilities from netWorthInfo where userID = '"+ ID +"';";
-
-	db.task('get-everything', task => {
-    return task.batch([
-      task.any(user_email),
-      task.any(user_name),
-      task.any(net_worth),
-      task.any(total_assets),
-      task.any(total_liabilities)
-      ]);
-  })
-	.then(info => {
-    //res.redirect('/create_account');
-    console.log(user_email);
-    console.log('it worked!!!!!!!!!!!!!!!!');
-  })
-  .catch(error => {
-    //res.redirect('/create_account');
-    console.log('there was an error!!!');
-  })
-
-	if(selector1 != null)
-	{
-		send_text = "Your friend " + user_name + "has networth of " + net_worth + 
-		", total assets of " + total_assets + ", and total liabilities of " + total_liabilities + ".";
-	}
-	else if(selector2 != null)
-	{
-		send_text = "Your friend " + user_name + " has networth of " + net_worth + 
-		", total assets of " + total_assets + ".";
-	}
-	
-	else if(selector3 !=null)
-	{
-		send_text = "Your friend " + user_name + " has networth of " + net_worth +
-		".";
-	}
-
-	send_text += "\n Comment: " + targetcomment;
-		/*var targetemail = req.body
-	
-		var user_id = "select userID from userInfo where email = targetemail";
-	
-		var user_name = "select firstName from userInfo where email = targetemail" + ' ' + "select lastName from userInfo where email = targetemail";
-	
-		var send_text = '';"
-
-	if(document.getElementById("High").checked = true)
-	{
-		send_text = "Your friend " + user_name + " has networth of " + "select netWorth from netWorthInfo where userID = user_id" + 
-		", total assets of " + "select totalAssets from netWorthInfo where userID = user_id" + ", and total liabilities" + 
-		"select totalLiabilities from netWorthInfo where userID = user_id";
-	}
-	else if(document.getElementById("Medium").checked = true)
-	{
-		send_text = "Your friend " + user_name + " has networth of " + "select netWorth from netWorthInfo where userID = user_id" + 
-		", and total assets of " + "select totalAssets from netWorthInfo where userID = user_id" + ".";
-	}
-	else if(document.getElementById("Low").checked = true)
-	{
-		send_text = "Your friend " + user_name + " has networth of " + "select netWorth from netWorthInfo where userID = user_id" + 
-		".";
-	}
-	*/
-
-	var mailOptions = {
-	  from: 'netWorthAppShare@gmail.com',
-	  to: 'liru4968@colorado.edu',
-	  subject: 'Your friend '+ 'user_name' + ' share networth with you!',
-	  text: send_text
-	};
-
-	transporter.sendMail(mailOptions, function(error, info){
-	  if (error) {
-	  	res.render('share',{
-	  		failed: true
-	  	})
-	  	// 'alert-node'.alert('Email send failed')
-	    console.log(error);
-	  } else {
-	  	res.render('home',
-	  	{
-	  		share: true
-	  	})
-	  	//alert('email sent successfully')
-	    console.log('Email sent: ' + info.response);
-	  }
-	}); 
-})
 
 app.get('/login', function (req, res){
   res.render('login');
@@ -199,8 +54,7 @@ app.get('/home', function (req, res) {
   console.log(rows)
         res.render('home',{
       my_title: "Home Page",
-      data: rows,
-      share: false
+      data: rows
     })
         })
   .catch(function (err) {
@@ -215,7 +69,7 @@ app.get('/home', function (req, res) {
 
 
 app.get('/add', function (req, res){
-  var query = "select * from itemsentered where userid = '1';";
+  var query = "select * from itemsentered where userid = '"+ ID +"';";
   db.any(query)
     .then(function (rows) { 
   console.log(rows)
@@ -229,7 +83,7 @@ app.get('/add', function (req, res){
 
 
 app.get('/edit', function (req, res){
-  var query = "select * from itemsentered where userid = '1';";
+  var query = "select * from itemsentered where userid = '"+ ID +"';";
   db.any(query)
     .then(function (rows) { 
   console.log(rows)
@@ -239,16 +93,55 @@ app.get('/edit', function (req, res){
     })
   })
 })
-
+// helper functions that work with database
+  class table {
+    static storeAccount (use,pas,ema,repas, first, last){
+      return new Promise( (resolve, reject) => {
+        var re = /\S+@\S+\.\S+/;
+        if(!use || !pas || !ema || !repas || !first || !last){
+          const error = new Error('Please enter all box');
+          return reject(error);
+        }
+        if(!re.test(ema)){
+          const error = new Error('The email is invalid');
+          return reject(error);
+        }
+        if(repas != pas){
+          const error = new Error('repet passord does not match');
+          return reject(error);
+        }
+        if(use.length > 64){
+          const error = new Error('Username is too long');
+          return reject(error);
+        }
+        if(pas.length > 64){
+          const error = new Error('Password is too long');
+          return reject(error);
+        }
+        var q = "insert into userInfo (userID, firstName, lastName, username, password, email) values( (Select max(userID) + 1 from (select * from userInfo) a),$4, $5, $1, $2, $3)"
+        db.none(
+          q,
+          [use, pas, ema,first, first, last]
+        );
+        resolve();
+      });
+    }
+  }
 
 app.post('/create_account/add_user',function (req, res){
-
+  var first = req.body.first;
+  var last = req.body.last
   var usr = req.body.username;
   var pass = req.body.password;
   var email_ = req.body.email;
+  var repas = req.body.repassword;
   console.log(usr);
   console.log(pass);
   console.log(email_);
+  console.log(repas);
+  console.log(first);
+  console.log(last);
+
 
   //var user_query = 'SELECT * FROM userInfo;';
   var user_query = "SELECT username FROM userInfo WHERE username = '"+ usr + "';";
@@ -256,17 +149,31 @@ app.post('/create_account/add_user',function (req, res){
 
   db.task('get-everything', task => {
     return task.batch([
-      task.any(user_query),
-      task.any(email_query)
+      task.none(user_query),
+      task.none(email_query)
       ]);
   })
   .then(info => {
-    res.redirect('/create_account');
-    console.log('it worked!!!!!!!!!!!!!!!!');
+    return table.storeAccount( usr, pass, email_, repas, first, last );
+  })
+  .then( () => {
+    var query = "SELECT username, password, userID FROM userInfo WHERE username = '" + usr + "' AND password = '" + pass + "';";
+      return db.any(query);
+  })
+  .then(rows => {
+    if(rows.length == 1){
+      ID = rows[0].userid;
+      res.redirect('/home');
+    }
+    else{
+      res.redirect('/login');
+    }
   })
   .catch(error => {
     res.redirect('/create_account');
-    console.log('there was an error!!!');
+    //popup.alert({content: 'Username or Email is already exist'});
+    console.log('there is an error');
+    throw (error);
   })
 })
 
@@ -277,13 +184,14 @@ app.post('/login/verify', function (req, res){
   db.any(query)
     .then(function (rows) { 
         if(rows.length == 1){
-      ID = rows[0].userid;
-      res.redirect('/home');
-  }
-  else{
-    res.redirect('/login');
-  }
-})
+          console.log(ID);
+          ID = rows[0].userid;
+          res.redirect('/home');
+        }
+        else{
+          res.redirect('/login');
+        }
+    })
 })
 
 
@@ -335,6 +243,204 @@ app.post('/create_account', function (req, res){
   });
 });
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/share', function (req, res){
+  res.render('share',{
+    failed : false
+  })
+})
+
+
+
+
+
+
+
+app.post('/share', function (req, res){
+
+  var selector1 = null;
+  var selector2 = null;
+  var selector3 = null;
+
+  if (req.body.selector1 != undefined)
+  {
+    selector1 = req.body.selector1;
+  }
+
+  if (req.body.selector2 != undefined)
+  {
+    selector2 = req.body.selector2;
+  }
+
+  if (req.body.selector3 != undefined)
+  {
+    selector3 = req.body.selector3;
+  }
+
+//install nodemailer first
+//enter: npm install nodemailer  
+
+    var nodemailer = require('nodemailer'); 
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'netWorthAppShare@gmail.com', //We need to create a public email address to send the information;
+      pass: '123networth'
+    }
+  });
+
+  var targetemail = req.body.email;
+  var targetcomment = req.body.comment;
+
+  var send_text = "12345";
+  var user_email = "select email from userInfo where userID = '" + ID + "';" ;
+  var user_name_1 = "select firstName from userInfo where userID = '" + ID + "';" 
+  var user_name_2 = "select lastName from userInfo userID = '" + ID + "';";
+  var net_worth = "select netWorth from netWorthInfo where userID = '" + ID + "';";
+  
+  var total_assets = "select totalAssets from netWorthInfo where userID = '" + ID + "';";
+  var total_liabilities = "select totalLiabilities from netWorthInfo where userID = '"+ ID +"';";
+
+  var select = "select *, (SELECT totalLiabilities FROM netWorthInfo Where userID = 1), (select totalAssets from netWorthInfo where userID = 1), (select netWorth from netWorthInfo where userID  =1)from userInfo where userID = '" + ID + "';";
+
+  var a ='';
+  var b ='';
+  db.task('get-everything', task => {
+    return task.batch([
+      // task.any(user_email),
+      // //task.any(user_name_1),
+      // //task.any(user_name_2),
+      // task.any(net_worth),
+      // task.any(total_assets),
+      // task.any(total_liabilities)
+      task.one(select)
+      ]);
+  })
+  .then(info => {
+    //res.redirect('/create_account'); 
+    var {email}             = info[0];
+    var {firstname}         = info[0];
+    var {lastname}          = info[0];
+    var {networth}          = info[0];
+    var {totalAssets}       = info[0];
+    var {totalassets}       = info[0];
+    var {totalliabilities}  = info[0];
+    console.log(info[0]);
+    if(selector1 != null)
+    { 
+      console.log(firstname);
+      console.log(send_text);
+      send_text = "Your friend " + firstname  + " " + lastname + " has networth of " + networth + 
+      ", total assets of " + totalassets + ", and total liabilities of " + totalliabilities + ".";
+      console.log(send_text);
+      
+    }
+    else if(selector2 != null)
+    {
+      send_text = "Your friend " + firstname + " " + lastname + " has networth of " + networth + 
+      ", total assets of " + totalassets + ".";
+    }
+    
+    else if(selector3 !=null)
+    {
+      send_text = "Your friend " + firstname + " " + lastname + " has networth of " + networth +
+      ".";
+    } 
+
+    a = firstname;
+    b = lastname;
+    
+    send_text += "\n Comment: " + targetcomment;
+    var mailOptions = {
+      from: 'netWorthAppShare@gmail.com',
+      to: 'liru4968@colorado.edu',
+      //liru4968@colorado.edu
+      subject: 'Your friend '+ a + " " + b + ' share networth with you!',
+      text: send_text
+    };
+    return transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        res.render('share',{
+          failed: true
+        })
+        // 'alert-node'.alert('Email send failed')
+        //console.log(error);
+        console.log(mailOptions);
+      } else {
+        res.render('home',
+        {
+          share: true
+        })
+        //alert('email sent successfully')
+        console.log(mailOptions);
+        console.log('Email sent: ' + info.response);
+      }
+    }); 
+  })
+  .then(() => {
+
+  })
+  .catch(error => {
+    console.log('there was an error!!!');
+    //res.redirect('/create_account');
+    throw (error);
+    
+  })
+
+
+
+    /*var targetemail = req.body
+  
+    var user_id = "select userID from userInfo where email = targetemail";
+  
+    var user_name = "select firstName from userInfo where email = targetemail" + ' ' + "select lastName from userInfo where email = targetemail";
+  
+    var send_text = '';"
+
+  if(document.getElementById("High").checked = true)
+  {
+    send_text = "Your friend " + user_name + " has networth of " + "select netWorth from netWorthInfo where userID = user_id" + 
+    ", total assets of " + "select totalAssets from netWorthInfo where userID = user_id" + ", and total liabilities" + 
+    "select totalLiabilities from netWorthInfo where userID = user_id";
+  }
+  else if(document.getElementById("Medium").checked = true)
+  {
+    send_text = "Your friend " + user_name + " has networth of " + "select netWorth from netWorthInfo where userID = user_id" + 
+    ", and total assets of " + "select totalAssets from netWorthInfo where userID = user_id" + ".";
+  }
+  else if(document.getElementById("Low").checked = true)
+  {
+    send_text = "Your friend " + user_name + " has networth of " + "select netWorth from netWorthInfo where userID = user_id" + 
+    ".";
+  }
+  */
+
+
+
+  
+})
 
 
 app.listen(3000, function () {
