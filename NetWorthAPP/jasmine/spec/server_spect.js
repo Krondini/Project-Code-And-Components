a89c69784bf8dd8522922463d4b441fba33dfd09
NinoZhongzhiZhang <zhzh3560@colorado.edu>
2019-05-04 19:01:42 -0700

    jasmine enviroment

---
+++ NetWorthAPP/jasmine/spec/server_spect.js
@@ -0,0 +1,23 @@
+describe('server.js', () => {
+	it('abc' () => {
+		var {table} = require('../server.js');
+		var t = new table;
+		
+	})
+
+	//var server;
+
+
+	// beforeAll(()=>{
+	// 	server = require('../server.js');
+	// });
+
+	// describe('GET /', ()=>{
+	// 	var data ={};
+	// 	beforeAll((done)=>{
+	// 		Request.get("http://localhost:3000/", (error, response, body) => {
+	// 			data.title = response.statusCode
+	// 		});
+	// 	});
+	// });
+});
